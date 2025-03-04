c134b0fe932f7f7e02d245f1c3a32203cee9a20d
simzim <info@simzim.lt>
2025-03-04 10:00:18 +0200

    show dish list

--- classes/Menu.js
+++ classes/Menu.js
@@ -27,7 +27,32 @@ class Menu {
     return htmlContent;
     }
 
+    generateDishInnerHTML(){
+        let htmlContent = `<table>`
 
+        this.#categories.forEach(cat =>{
+            htmlContent += `
+                <tr>
+                    <th>Kategorija</th>
+                    <th colspan='2'>${cat.getCategoryName()}</th>
+                </tr>
+            `;
+            cat.getDishesList().forEach(dish => {
+                htmlContent += `
+                    <tr>
+                        <td>${dish.getName()}</td>
+                        <td>${dish.getPrice()} Eur</td>
+                        <td>${dish.getDescription()}</td>
+                    </tr>
+                
+                `
+            })
+        })
+        htmlContent += `</table>`
+        
+        return htmlContent;
+
+    }
 
 
 }
